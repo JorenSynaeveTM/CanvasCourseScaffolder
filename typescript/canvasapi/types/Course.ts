37cf1243a3d801e4785d08637af20308a596292e
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2024-02-06 11:54:44 +0100

    Add imageID

--- typescript/canvasapi/types/Course.ts
+++ typescript/canvasapi/types/Course.ts
@@ -11,6 +11,7 @@ class Course {
     name?: string
     syllabusBody?: string
     defaultView?: CourseDefaultView
+    imageId?: number
 
     constructor(id: number) {
         this.id = id;
@@ -21,6 +22,7 @@ class Course {
         if (course.name) formdata.append("course[name]", course.name);
         if (course.syllabusBody) formdata.append("course[syllabus_body]", course.syllabusBody);
         if (course.defaultView) formdata.append("course[default_view]", course.defaultView);
+        if (course.imageId) formdata.append("course[image_id]", course.imageId.toString());
         return formdata;
     }
 }
